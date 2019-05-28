622c8e7691598244127a9c053b5b4dba3922b5d0
Shomen Muhury <shomenmuhury@gmail.com>
2019-05-29 01:52:17 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Dummy React App
+<h2 style="text-align:center;">Dummy React App </h2>
 
 
 ## Created Using 
@@ -10,5 +10,5 @@
 ### Images used from - <a href="https://unsplash.com/" target="_blank">https://unsplash.com/</a>
 
 
-Check Demo here - <a href="https://htmlpreview.github.io/?https://github.com/Shomen/eshop/blob/master/index.html" target="_blank">Demo</a>
+Check Demo here - <a href="https://react-app-eshop.herokuapp.com/" target="_blank">Demo</a>
 
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/Shomen/eshop/blob/master/precache-manifest.bd9d0bcbbb17fc9cab75634c950de4f1.js"
+  "/precache-manifest.dbb19fccc6aca05034fa30d3eacfd83f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Shomen/eshop/blob/master/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
